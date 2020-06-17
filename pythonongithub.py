b12b5bef85bbcd3ef9eb234ff4d496db1b229e07
Raymond Mar <raym61@hotmail.com>
2020-06-16 20:50:02 -0700

    Added an if statement

--- pythonongithub.py
+++ pythonongithub.py
@@ -1,2 +1,9 @@
 #Create a Python file on GitHub
 print("Hello World")
+dayvariable = "Tue"
+if dayvariable == "Tue":
+    print("Today is Tue")
+elif dayvariable == "Fri":
+    print("See you on Fri")
+else:
+    print("Have a good day")
